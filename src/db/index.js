77d51e9d965f78bd537fdfde9ac2e40fe6ef55ad
Luv3-ICE <magna12567@gmail.com>
2026-01-28 16:21:03 +0700

    Add files via upload

--- src/db/index.js
+++ src/db/index.js
@@ -9,8 +9,13 @@ import pg from "pg";
 // });
 export const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 pool.on("connect", () => {
   console.log("🟢 PostgreSQL connected");
 });
+
