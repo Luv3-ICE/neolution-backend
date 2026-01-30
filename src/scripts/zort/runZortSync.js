4ae104f910fbbd89ac5616184e6bf64817628d25
Luv3-ICE <magna12567@gmail.com>
2026-01-30 11:18:28 +0700

    Add files via upload

--- src/scripts/zort/runZortSync.js
+++ src/scripts/zort/runZortSync.js
@@ -4,7 +4,7 @@ import saveZortDB from "./saveZortToDB.js";
 export async function runZortSync() {
   console.log("🔄 Start Zort Sync");
 
-  const orders = await fetchZortOrders();
+  const orders = await fetchZortSnapshot();
   console.log(`📦 Orders from Zort: ${orders.length}`);
 
   await saveZortDB(orders);
