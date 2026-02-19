07c55dcda4f319032820a8a5267fc5939d834206
Luv3-ICE <magna12567@gmail.com>
2026-02-19 16:32:33 +0700

    Refactor order.js to use named import for pool

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,5 +1,5 @@
 import express from "express";
-import pool from "../db/index.js";
+import { pool } from "../db/index.js";
 import fetch from "node-fetch";
 
 const router = express.Router();
@@ -97,3 +97,4 @@ router.post("/checkout", async (req, res) => {
 });
 
 export default router;
+
